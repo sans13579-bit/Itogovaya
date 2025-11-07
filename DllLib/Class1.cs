203d9a83b0ab295ee47f539eb5b6943701479581
Dmitry <crimsons032@gmail.com>
2025-11-07 10:58:01 +0900

    first commit

---
+++ DllLib/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DllLib
+{
+    public class Class1
+    {
+        public void GetChiefLibrarian(string name) { Console.WriteLine($"Имя текущего главного библеотекаря {name}"); }
+        public void GetLibName(string name) { Console.WriteLine($"Название библеотеки {name}"); }
+        public string SetChiefLibrarian(string name, List<string> sotrud) 
+        {
+            Console.WriteLine("Список сотрудников");
+            Console.WriteLine(string.Join(",",sotrud));
+            Console.WriteLine("Введите имя нового главного библеотекаря");
+            string newname = Console.ReadLine();
+            if (sotrud.Contains(newname)) { return  newname; }
+            else { return name = "Такого имени нет в списке сотрудников"; }
+        }
+        public string GenerateBookCode(List<string> category) 
+        {
+            Random random = new Random();
+            int number = random.Next(0,100);
+            string randomCat = category[random.Next(category.Count)];
+            return $"{randomCat}_{number}";
+        }
+        public void GetStaff(List<string> sotrud) 
+        {
+            Console.WriteLine("Список сотрудников");
+            Console.WriteLine(string.Join(",", sotrud)); 
+        }
+        public List<string> AddStaff(string newmemb, List<string> sotrud) 
+        {
+            sotrud.Add(newmemb);
+            Console.WriteLine("Список сотрудников");
+            Console.WriteLine(string.Join(",", sotrud));
+            return sotrud;
+        }
+    }
+    
+}
---
+++ Itogovaya/Program.cs
@@ -0,0 +1,55 @@
+﻿using DllLib;
+int count = 0;
+Class1 deist = new Class1();
+string free,randombook,newstaff;
+string LibName = "Городская библиотека г. Чита";
+string GlavBib = "Иванова Мария Петровна";
+List<string> sotrud = new List<string>
+{
+     "Петров П.С.","Сидорова А.В.","Козлов И.Д.","Иванова М.П."
+};
+List<string> cat = new List<string>
+{"Научная книга","Научно-популярная книга","Производственно-практическая книга",
+"Учебная книга","Справочная книга","Информационная книга",
+};
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("Выберите действие \n 1 - Вывести ФИО главного библиотекаря \n 2 - Вывести название библиотеки \n 3 - Вывод сотрудников библиотеки \n 4 - Установить нового главного билиотекаря \n 5 - Сгенерировать книгу  \n 6 - Добавить сотрудника");
+    try
+    {
+        int c = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        switch (c)
+        {
+            case 1:
+                deist.GetChiefLibrarian(GlavBib);
+                break;
+            case 2:
+                deist.GetLibName(LibName);
+                break;
+            case 3:
+                deist.GetStaff(sotrud);
+                break;
+            case 4:
+                GlavBib = deist.SetChiefLibrarian(LibName, sotrud);
+                Console.Clear();
+                if (GlavBib.Contains($"Такого"))
+                { Console.WriteLine("Такого имени нет в списке сотрудников"); }
+                else { Console.WriteLine($"Новый главный библиотекарь " + GlavBib); }
+                break;
+            case 5:
+                randombook = deist.GenerateBookCode(cat);
+                Console.WriteLine(randombook);
+                break;
+            case 6:
+                Console.WriteLine("Введите имя нового сотрудника");
+                newstaff = Console.ReadLine();
+                deist.AddStaff(newstaff, sotrud);
+                break ;
+        }
+}
+    catch (System.FormatException) { Console.WriteLine("Не число"); }
+
+}
+while (count == 0);
